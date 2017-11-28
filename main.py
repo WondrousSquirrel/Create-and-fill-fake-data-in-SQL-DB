b3a01a1790afcba9987b401d23bbbdba00e068d9
alexzenderA <alex_zender@i.ua>
2017-11-28 21:58:28 +0200

    add random

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 '''Execute script and fill fake data in db'''
+import random
 from sql_db import *
 from mimesis import Generic
 from sqlalchemy.orm import sessionmaker
 
 
 # write type of connection - sqlite/mysql, database_name
-db = connect_to_db('mysql', 'drink_magazine')
+db = connect_to_db('sqlite', 'drink_magazine')
 
 if __name__ == '__main__':
 
@@ -18,14 +19,17 @@ if __name__ == '__main__':
     g = Generic()
 
     for i in range(1, 11):
+        disc = random.random()
+        price = random.randint(10, 50)
+        quantity = random.randint(1, 10)
         supp = Suppliers(CompanyName=g.business.company(), ContactName=g.personal.full_name(),
                          Adress=g.address.street_name() + ' ' + g.address.street_suffix(),
                          Phone=g.personal.telephone(), Country=g.address.country()
                         )
         orders = Orders(CustomerID=i, ShipName=g.food.drink(), ShipAdress=g.address.state())
         category = Categories(CategoryName=g.food.drink(), Description=g.text.sentence())
-        product = Products(SupplierID=i, CategoryID=i, ProductName=g.personal.name(), UnitPrice=10)
-        orderDet = OrderDetail(ProductID=i, UnitPrice=15, Quantity=1, Discount=1.5)
+        product = Products(SupplierID=i, CategoryID=i, ProductName=g.personal.name(), UnitPrice=price)
+        orderDet = OrderDetail(ProductID=i, UnitPrice=price, Quantity=quantity, Discount=disc)
         session.add_all([supp, orders, category, product, orderDet])
 
     session.commit()
